d96a156935c394650072f419676d57f5b62c9835
z <z>
2018-10-13 23:02:33 +0300

    commit to master

--- WebApiLoad/Controllers/ValuesController.cs
+++ WebApiLoad/Controllers/ValuesController.cs
@@ -18,6 +18,12 @@ namespace WebApiLoad.Controllers
         }
 
         [SwaggerOperation("GetString")]
+        public IEnumerable<string> Get1(string str)
+        {
+            return new string[] { str };
+        }
+
+        [SwaggerOperation("GetString")]
         public IEnumerable<string> Get2(string str)
         {
             return new string[] { str };
@@ -25,12 +31,6 @@ namespace WebApiLoad.Controllers
 
         [SwaggerOperation("GetString")]
         public IEnumerable<string> Get3(string str)
-        {
-            return new string[] { str };
-        }
-
-        [SwaggerOperation("GetString")]
-        public IEnumerable<string> Get4(string str)
         {
             return new string[] { str };
         }
