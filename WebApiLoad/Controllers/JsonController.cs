309a90e7cf61251f08b21c7763fb739535fd77ac
z <z>
2018-10-13 19:15:18 +0300

    inital project

---
+++ WebApiLoad/Controllers/JsonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApiLoad.Controllers
+{
+    public class JsonController : ApiController
+    {
+        [HttpPost]
+        public async Task<object> Post()
+        {
+            var body = await Request.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            var val = Newtonsoft.Json.JsonConvert.DeserializeObject<object>(body);
+            return  val;
+        }
+    }
+}
---
+++ WebApiLoad/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiLoad.WebApiApplication" Language="C#" %>
