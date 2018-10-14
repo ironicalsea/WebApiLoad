b58347e5825b2191d7bbe75ccbf6e643c13816d7
z <z>
2018-10-14 14:35:48 +0300

    change to 123

--- WebApiLoad/Controllers/ValuesController.cs
+++ WebApiLoad/Controllers/ValuesController.cs
@@ -29,7 +29,7 @@ namespace WebApiLoad.Controllers
         [SwaggerResponse(HttpStatusCode.NotFound)]
         public string Get(int id)
         {
-            return "value1234";
+            return "value123";
         }
 
         // POST api/values
